fc80fc81835ee7adaeadbcae6e003131dc68effa
Tyrell Blackburn <tyrell.blackburn@gmail.com>
2021-02-17 19:25:37 +0800

    up to lecture 125

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import Burgerbuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Burgerbuilder/>
+        </Layout>
+      </div> 
+    );
+  }
 }
 
 export default App;
