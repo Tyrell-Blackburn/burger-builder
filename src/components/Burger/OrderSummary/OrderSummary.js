ba4de6b92c1c446d065958c03a3eff988018418e
Tyrell Blackburn <tyrell.blackburn@gmail.com>
2021-02-28 14:10:12 +0800

    up to lecture 161 starting to learn about Ajax

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
+class OrderSummary extends Component {
+    // this could be a functional component, was just changed to class to debut with componentDidUpdate
+    componentDidUpdate () {
+        console.log('[OrderSummary] WillUpdate');
+    }
 
-    const ingredientSummary = Object.keys(props.ingredients).map(ingredient => {
+    render() {
+
+        const ingredientSummary = Object.keys(this.props.ingredients).map(ingredient => {
+            return (
+                <li key={ingredient}>
+                    <span style={{textTransform: 'capitalize'}}>{ingredient}</span>
+                    : {this.props.ingredients[ingredient]}
+                </li>
+            ) 
+        })
+
         return (
-            <li key={ingredient}>
-                <span style={{textTransform: 'capitalize'}}>{ingredient}</span>
-                : {props.ingredients[ingredient]}
-            </li>
-        ) 
-    })
-
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burget with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price : ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to Check Out</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>Continue</Button>
-        </Aux>
-    );
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burget with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price : ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Check Out</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>Continue</Button>
+            </Aux>
+        );
+    };
 };
 
-export default orderSummary;
+export default OrderSummary;
